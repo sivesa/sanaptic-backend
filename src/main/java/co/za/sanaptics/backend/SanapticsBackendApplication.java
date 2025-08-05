70934ebfd214f633660d6a4e4bf1b96e80ce7eaa
Sive <sivesandla@live.com>
2025-08-05 13:06:32 +0200

    Initial spring initialzr.

---
+++ src/main/java/co/za/sanaptics/backend/SanapticsBackendApplication.java
@@ -0,0 +1,13 @@
+package co.za.sanaptics.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanapticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanapticsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/sanaptics/backend/SanapticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.sanaptics.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanapticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
